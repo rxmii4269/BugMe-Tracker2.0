82da03e0a901e83facf195f7e7f594b4105616b2
Lincoln-Brown <links1155@gmail.com>
2019-11-27 12:45:33 -0500

    working progress on post request

--- bugme_newuser.js
+++ bugme_newuser.js
@@ -1,35 +1,75 @@
 function Main(){
-//var newuser=document.querySelector('#newuserbtn');
+var errorcolor="#E45449";
+var newuser=document.querySelector('#newuserbtn');
+var first = document.getElementById('firstname');
+var last = document.getElementById('lastname');
+var pw = document.getElementById('password');
+var email = document.getElementById('email');
+var nameexp=/[a-z]/;
+var pwexp =/[a-zA-Z0-9]+/;//need to fix[a-zA-z0-9]+\@[a-z]+\.[a-z]{3}
+var emailexp=/[a-zA-z0-9]+\@[a-z]+\.[a-z]{3}/
 //var newissue=document.querySelector('#new_issuebtn');
 
-/*newuser.onclick=function(){
-	alert("sub user");
+newuser.onclick=function(){
+	
+	
+	if ((nameexp.test(first.value)&& nameexp.test(last.value) && emailexp.test(email.value))&&
+		(pwexp.test(pw.value) && pw.value.length>=8)) {
+		
+
+	$.ajax({
+		type:"POST",
+		url:"database.php",
+		data: $('#user').serialize(),
+		success:function(data){alert (data)}
 
 
+	});
 
+alert('form validated');
+
+	} else{
+		if(nameexp.test(first.value)==false){
+			var nt=document.getElementById('firstname');
+			
+			nt.style.backgroundColor=errorcolor;
+
+
+		}
+		if(nameexp.test(last.value)==false){
+			var nt=document.getElementById('lastname');
+			
+			nt.style.backgroundColor=errorcolor;
+
+		}
+
+		if(emailexp.test(email.value)==false){
+			var nt=document.getElementById('email');
+			
+			nt.style.backgroundColor=errorcolor;
+
+		}
+		if((pwexp.test(pw.value)==false || pw.value.length<8)){
+			var nt=document.getElementById('password');
+			
+			nt.style.backgroundColor=errorcolor;
+
+
+		}
+
+
+		alert('not validated remember to fix reg expression');
 	}
+	//alert(email.value);
+	//alert( first.value,last.value, pw.value, email.value);
 
-
-newissue.onclick=function(){
-	alert("issue");
-
-
-
-	}
-*/
+}
 
 
 
 
 
-
-
-
-
-
-
-
-	alert("loaded");
+		alert("loaded");
 }
 
 
--- database.php
+++ database.php
@@ -1,4 +1,19 @@
 <?php
+//echo htmlentities($_POST['description']);
+
+function text(){
+
+	echo"test";
+}
+
+if (array_key_exists( 'i', $_POST )){
+	//echo htmlentities($_POST['description']);
+	text();
+
+}else{
+	echo var_dump($_POST);
+	//echo htmlentities($_POST['description']);
+}
 
 $host = '54.84.172.164';
 $username = 'bugMe';
