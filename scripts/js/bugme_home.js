86ad11f9be2e9cbfdc4bb7318d0de5a7d01bc9a7
Romaine Murray <romainemurray6@gmail.com>
2019-11-29 07:15:54 -0500

    main buttons are now fully ajax compliant
    Signed-off-by: Romaine Murray <romainemurray6@gmail.com>

--- scripts/js/bugme_home.js
+++ scripts/js/bugme_home.js
@@ -5,9 +5,10 @@ window.onload = () => {
 
 	let home = document.getElementById('home');
 	let issue = document.getElementById('newissue');
+	let adduser = document.getElementById('adduser');
+	let logout = document.getElementById('');
 
-
-	home.addEventListener('click',()=>{
+	home.addEventListener('click', () => {
 		this.console.log("home clicked");
 	});
 
@@ -15,9 +16,17 @@ window.onload = () => {
 	issue.addEventListener('click', () => {
 		this.console.log("btn-clicked");
 
-		this.$('#center').load("new_issue.html #center");
+		this.$('#center').load("new_issue.html ");
 	});
 
 
+	adduser.addEventListener('click', () => {
+		this.console.log('userclicked');
+		this.$('#center').load('new_user.html');
+	});
+
+	logout.addEventListener('click',()=>{
+		this.$('#center').load('index.html');
+	});
 
 };
