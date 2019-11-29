fd6d73dfd11344324035db5d886b58cb57e0cdeb
Romaine Murray <romainemurray6@gmail.com>
2019-11-29 17:43:26 -0500

    implemented ajax request and sql for new user
    Signed-off-by: Romaine Murray <romainemurray6@gmail.com>

--- scripts/js/bugme_home.js
+++ scripts/js/bugme_home.js
@@ -6,7 +6,9 @@ window.onload = () => {
 	let home = document.getElementById('home');
 	let issue = document.getElementById('newissue');
 	let adduser = document.getElementById('adduser');
-	let logout = document.getElementById('');
+	let logout = document.getElementById('logout');
+
+
 
 	home.addEventListener('click', () => {
 		this.console.log("home clicked");
@@ -22,11 +24,45 @@ window.onload = () => {
 
 	adduser.addEventListener('click', () => {
 		this.console.log('userclicked');
-		this.$('#center').load('new_user.html');
+		this.$('#center').load('new_user.html', () => {
+
+			var newuser = document.getElementById('newuserbtn');
+			var errorcolor = "#E45449";
+			var first = document.getElementById('firstname');
+			var last = document.getElementById('lastname');
+			var pw = document.getElementById('password');
+			var email = document.getElementById('email');
+			var nameexp = /[a-z]/;
+			var pwexp = /[a-zA-Z0-9]+/;
+			var emailexp = /[a-zA-z0-9]+\\@[a-z]+\.[a-z]{3}/;
+			// this.$.getScript("/scripts/js/bug_newuser.js");
+			this.console.log(first);
+			newuser.addEventListener('click', (e) => {
+				e.preventDefault();
+				this.$.ajax({
+					type: 'POST',
+					url:"scripts/php/bugMe.php",
+					data:{'firstname':first.value,'lastname':last.value,'password':pw.value,
+					'email':email.value,"new_userbtn":newuser.value},
+					success:(data)=>{
+						this.alert(data);
+					},
+					dataType:'html'
+
+				});
+				
+				
+
+
+			});
+		});
+
+
+
 	});
 
-	logout.addEventListener('click',()=>{
-		this.$('#center').load('index.html');
+	logout.addEventListener('click', () => {
+		window.location.href = "/";
 	});
 
 };
--- scripts/php/bugMe.php
+++ scripts/php/bugMe.php
@@ -1,34 +1,62 @@
 <?php
-//session_start();
-
-include_once('database.php');
 
 
-$email = filter_var(htmlspecialchars($_POST['email']), FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL);
-$password = $_POST['password'];
 
-$stmt = $pdo->prepare("SELECT * FROM Users WHERE email = ? ");
+function newuser()
+{
+    include('database.php');
+    if($_SERVER["REQUEST_METHOD"] === 'POST'):
+        $firstname = filter_var(htmlspecialchars($_POST['firstname']),FILTER_SANITIZE_STRING);
+        $lastname = filter_var(htmlspecialchars($_POST['lastname']),FILTER_SANITIZE_STRING);
+        $password = $_POST['password'];
+        $email = filter_var(htmlspecialchars($_POST['email']), FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL);
 
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") :
-    if ($email != '' && $password != '') :
-        $stmt->execute([$email]);
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        $db_password = $results[0]['password'];
-        $hashed_password = hash("sha256", $password);
+        $stmt = $pdo->prepare("INSERT INTO Users (firstname,lastname,password,email,date_joined) VALUES(?,?,SHA2(?,0),?,CURDATE())");
+        $stmt->execute([$firstname,$lastname,$password,$email]);
+        echo "User saved successfully";
+    endif;
+}
 
-        if ($db_password === $hashed_password) :
-            //echo var_dump("passwords match");
-            session_start();
-            $_SESSION['login_user'] = $email;
-            echo "success";
+function login()
+{
+    include('database.php');
+    $email = filter_var(htmlspecialchars($_POST['email']), FILTER_VALIDATE_EMAIL, FILTER_SANITIZE_EMAIL);
+    $password = $_POST['password'];
+
+    $stmt = $pdo->prepare("SELECT * FROM Users WHERE email = ? ");
+
+
+    if ($_SERVER["REQUEST_METHOD"] === "POST") :
+        if ($email != '' && $password != '') :
+            $stmt->execute([$email]);
+            $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $db_password = $results[0]['password'];
+            $hashed_password = hash("sha256", $password);
+
+            if ($db_password === $hashed_password) :
+                //echo var_dump("passwords match");
+                session_start();
+                $_SESSION['login_user'] = $email;
+                echo "success";
+            else :
+                var_dump($results[0]['password']);
+                var_dump(hash('sha256', $password));
+            endif;
         else :
-            var_dump($results[0]['password']);
-            var_dump(hash('sha256', $password));
+            echo "Login form cannot be empty";
         endif;
     else :
-        echo "Login form cannot be empty";
+        echo "Method cannot be post";
     endif;
-else :
-    echo "Method cannot be post";
+}
+
+
+if (isset($_POST["new_userbtn"])) :
+    newuser();
+
+elseif (isset($_POST["login_submit"])) :
+    login();
+
+
 endif;
