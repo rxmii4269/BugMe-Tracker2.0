ac481643f5d27178b309efb705b3852e4e8133d2
rxmii4269 <romainemurray6@gmail.com>
2020-09-11 12:58:39 -0500

    revert commit

--- scripts/php/database.php
+++ scripts/php/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'd1kb8x1fu8rhcnej.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
-$username = 'rr2spmhdp6sb2vxa';
-$password = 'mtm23flbqkf1usk5';
-$dbname = 'qct0nyo6v3qvg60y';
+$host = 'localhost';
+$username = 'bugMe';
+$password = 'iNFO@2180';
+$dbname = 'bugMe_Tracker';
 
 
 $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4",$username,$password);
