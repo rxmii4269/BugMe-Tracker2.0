2d89076c0d63eadb81d88cedb76e0c3fe723aff7
rxmii4269 <romainemurray6@gmail.com>
2020-09-11 11:55:16 -0500

    testing new heroku mysql db

--- scripts/php/database.php
+++ scripts/php/database.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = 'localhost';
-$username = 'bugMe';
-$password = 'iNFO@2180';
-$dbname = 'bugMe_Tracker';
+$host = 'mysql://bb94ec1873173c:e48319e2@us-cdbr-east-02.cleardb.com/heroku_87fbc08c29a250e?reconnect=true';
+$username = 'bb94ec1873173c';
+$password = 'e48319e2';
+$dbname = 'heroku_87fbc08c29a250e';
 
 
 $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4",$username,$password);
